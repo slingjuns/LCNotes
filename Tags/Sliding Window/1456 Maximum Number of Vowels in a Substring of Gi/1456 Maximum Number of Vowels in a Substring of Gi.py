630e3d7499cbf15533683db8c4e93388b10ff45a
slingjun <slingjuns@gmail.com>
2021-11-12 00:00:00 -0500

    Commit 1456 Maximum Number of Vowels in a Substring of Gi.py

---
+++ Tags/Sliding Window/1456 Maximum Number of Vowels in a Substring of Gi/1456 Maximum Number of Vowels in a Substring of Gi.py
@@ -0,0 +1,12 @@
+// Date: November 12, 2021
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        ans = cnt = 0
+        for i, c in enumerate(s):
+            if c in vowels:
+                cnt += 1
+            if i >= k and s[i - k] in vowels:
+                cnt -= 1
+            ans  = max(cnt, ans)
+        return ans

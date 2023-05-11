54d773ead9094fc228fcd6044b1fb8bf65bf51ca
slingjun <slingjuns@gmail.com>
2023-05-11 15:32:49 -0400

    Commit 236 Lowest Common Ancestor of a Binary Tree.py

---
+++ Tags/LCA/236 Lowest Common Ancestor of a Binary Tree/236 Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right: # the lowest common ancestor
+            return root
+        return left or right

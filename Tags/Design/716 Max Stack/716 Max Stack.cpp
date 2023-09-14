a5cf14d1ec07eb52a0538f6ddf56ced7304e0f0b
slingjun <slingjuns@gmail.com>
2023-09-14 00:00:00 -0400

    Commit 716 Max Stack.cpp

---
+++ Tags/Design/716 Max Stack/716 Max Stack.cpp
@@ -0,0 +1,49 @@
+// Date: September 14, 2023
+class MaxStack {
+private:
+    list<int> st;
+    map<int, vector<list<int>::iterator>> m;   // value -> iterator
+public:
+    MaxStack() {
+    }
+    
+    void push(int x) {
+        st.push_back(x);
+        m[x].push_back(--st.end());
+    }
+    
+    int pop() {
+        auto x = st.back();
+        st.pop_back();
+        if(m[x].size() == 1) m.erase(x);
+        else m[x].pop_back();
+        return x;
+    }
+    
+    int top() {
+        return st.back();
+    }
+    
+    int peekMax() {
+        return (--m.end())->first;
+    }
+    
+    int popMax() {
+        auto x = (--m.end())->first;
+        list<int>::iterator target = m[x].back();
+        if(m[x].size() == 1) m.erase(x);
+        else m[x].pop_back();
+        st.erase(target);
+        return x;
+    }
+};
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack* obj = new MaxStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->peekMax();
+ * int param_5 = obj->popMax();
+ */

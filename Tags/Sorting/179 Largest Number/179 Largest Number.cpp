54ae4c10af8f7f4eb270551ddebc56aa965bb977
slingjun <slingjuns@gmail.com>
2023-10-01 00:00:00 -0400

    Commit 179 Largest Number.cpp

---
+++ Tags/Sorting/179 Largest Number/179 Largest Number.cpp
@@ -0,0 +1,17 @@
+// Date: October 1, 2023
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> ss;
+        for(auto num: nums) ss.push_back(to_string(num));
+        sort(ss.begin(), ss.end(), [](const string& s1, const string& s2) {
+            return s1 + s2 > s2 + s1;
+        });
+        
+        string res;
+        for(auto s: ss) res += s;
+        while(res[0]=='0' && res.length()>1)
+            res.erase(0,1);
+        return res;
+    }
+};

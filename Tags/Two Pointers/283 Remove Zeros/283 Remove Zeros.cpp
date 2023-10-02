5741e143442d7bc5ed66077c9df07ead391efd5a
slingjun <slingjuns@gmail.com>
2023-10-02 00:00:00 -0400

    Commit 283 Remove Zeros.cpp

---
+++ Tags/Two Pointers/283 Remove Zeros/283 Remove Zeros.cpp
@@ -0,0 +1,10 @@
+// Date: October 2, 2023
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] != 0) swap(nums[left++], nums[i]);
+        }
+    }
+};

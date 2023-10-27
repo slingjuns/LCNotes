a511afc8fbb19918284f9f812cced1a36aeda425
slingjun <slingjuns@gmail.com>
2023-10-27 00:00:00 -0400

    Commit 735 Asteroid Collision.cpp

---
+++ Tags/Stack/735 Asteroid Collision/735 Asteroid Collision.cpp
@@ -0,0 +1,26 @@
+// Date: October 27, 2023
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        // stack
+        vector<int> st;
+        for(auto ast:asteroids) {
+            // colliding is popping elements in stack until no more colliding
+            bool push_cur = true;
+            while(!st.empty() && (st.back() > 0 && ast < 0)) {
+                if(abs(st.back()) < abs(ast)) st.pop_back();
+                else if(abs(st.back()) == abs(ast)) { // both explode
+                    st.pop_back();
+                    push_cur = false;
+                    break;
+                } else { // ast explode
+                    push_cur = false;
+                    break;
+                }
+            }
+            if(push_cur) st.push_back(ast);
+        }
+        
+        return st;
+    }
+};

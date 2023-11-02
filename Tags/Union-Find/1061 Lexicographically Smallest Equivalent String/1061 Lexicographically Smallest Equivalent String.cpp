08929ffbe66e34c737eb02b461bef2e23b3c9a7e
slingjun <slingjuns@gmail.com>
2023-11-02 00:00:00 -0400

    Commit 1061 Lexicographically Smallest Equivalent String.cpp

---
+++ Tags/Union-Find/1061 Lexicographically Smallest Equivalent String/1061 Lexicographically Smallest Equivalent String.cpp
@@ -0,0 +1,33 @@
+// Date: November 2, 2023
+class Solution {
+    class UF {
+    public:
+        vector<int> fa;
+        UF(int n) {
+            fa.resize(n);
+            for(int i = 0; i < n; ++i) fa[i] = i;
+        }
+        
+        int find(int x) {
+            return fa[x] == x ? x : fa[x] = find(fa[x]);
+        }
+        
+        void Union(int x, int y) {
+            int fa_x = find(x), fa_y = find(y);
+            if(fa_x < fa_y) fa[fa_y] = fa_x;
+            else fa[fa_x] = fa_y;
+        }
+    };
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        UF uf(26);
+        for(int i = 0; i < s1.length(); ++i) {
+            uf.Union(s1[i]-'a', s2[i]-'a');
+        }
+        
+        for(char &ch: baseStr) {
+            ch = uf.find(ch-'a') + 'a';
+        }
+        return baseStr;
+    }
+};

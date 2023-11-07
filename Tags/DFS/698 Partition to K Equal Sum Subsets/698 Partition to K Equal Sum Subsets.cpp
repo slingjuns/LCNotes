591dfd9f2db32dffddf654ae42c65f2eb6b20c19
slingjun <slingjuns@gmail.com>
2023-11-07 00:00:00 -0500

    Commit 698 Partition to K Equal Sum Subsets.cpp

---
+++ Tags/DFS/698 Partition to K Equal Sum Subsets/698 Partition to K Equal Sum Subsets.cpp
@@ -0,0 +1,45 @@
+// Date: November 7, 2023
+class Solution {
+int total;
+int k;
+vector<int> bucket;
+public:
+bool canPartitionKSubsets(vector<int>& nums, int k) {
+    bucket.resize(k);
+    if (k > nums.size()) return false;
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    if (sum % k != 0) return false;
+
+    vector<int> bucket(k);
+    int target = sum / k;
+    this->k = k;
+    sort(nums.begin(), nums.end(), greater<int>());
+    return dfs(nums, 0, 0, target);
+}
+
+bool dfs(vector<int>& nums, int index, int used, int target) {
+    if (index == nums.size()) {
+        for (int i = 0; i < used; i++) {
+            if (bucket[i] != target) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    for (int i = 0; i < used; i++) {
+        if (bucket[i] + nums[index] > target) continue;
+        bucket[i] += nums[index];
+        if (dfs(nums, index + 1, used, target)) return true;
+        bucket[i] -= nums[index];
+    }
+    // 新开一个
+    if(used+1 <= k) {
+        bucket[used] += nums[index];
+        if(dfs(nums, index+1, used+1, target)) return true;
+        bucket[used] -= nums[index];
+    }
+
+    return false;
+}
+};

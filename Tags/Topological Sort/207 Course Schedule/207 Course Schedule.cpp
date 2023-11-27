957a6a53e1bc66f70a8197822f1fd70b138b7acf
slingjun <slingjuns@gmail.com>
2023-11-27 00:00:00 -0500

    Commit 207 Course Schedule.cpp

---
+++ Tags/Topological Sort/207 Course Schedule/207 Course Schedule.cpp
@@ -0,0 +1,35 @@
+// Date: November 27, 2023
+class Solution {
+public:
+vector<int> st;
+vector<vector<int>> edge;//邻接表
+    
+bool dfs(int u) {
+    //标识u结点正在访问
+    st[u] = 1;
+
+    //遍历每个出边,找到下一组结点
+    for (int v:edge[u]) {
+        //如果遇到了正在访问的结点，那么说明有环
+        if (st[v] == 1) return true;
+        //如果v这个结点没有访问过，递归查找v结点是否在环中
+        if (st[v] == 0 && dfs(v)) return true;
+    }
+
+    //标识u结点访问完毕
+    st[u] = -1;
+    return false;
+}
+    bool canFinish(int numCourses, vector<vector<int>>& prereq) {
+        st.resize(numCourses);
+        edge.resize(numCourses);
+        for(auto& course: prereq) {
+            edge[course[0]].push_back(course[1]);
+        }
+         //将所有未访问过的结点进行深入优先搜索判断是否有环
+        for (int i = 0; i < numCourses; i++)
+            if (!st[i] && dfs(i))//没有访问过，并且有环
+                return false;
+        return true;
+    }
+};

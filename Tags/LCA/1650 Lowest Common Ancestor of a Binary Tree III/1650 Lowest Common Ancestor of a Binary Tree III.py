bc8480e9bf321bbc52332078b13d78c3af632d89
slingjun <slingjuns@gmail.com>
2024-01-16 15:32:49 -0500

    Commit 1650 Lowest Common Ancestor of a Binary Tree III.py

---
+++ Tags/LCA/1650 Lowest Common Ancestor of a Binary Tree III/1650 Lowest Common Ancestor of a Binary Tree III.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        a, b = p, q
+        while a != b:
+            a = a.parent if a.parent else q
+            b = b.parent if b.parent else p
+        return a
+

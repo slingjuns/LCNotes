9c97aa3532dd659688fe94fc138c934adfe5d5ae
slingjun <slingjuns@gmail.com>
2024-01-25 15:32:49 -0500

    Commit 863 All Nodes Distance K in Binary Tree.py

---
+++ Tags/Graph/863 All Nodes Distance K in Binary Tree/863 All Nodes Distance K in Binary Tree.py
@@ -0,0 +1,35 @@
+class Solution:
+    def distanceK(self, root, target, K):
+        conn = collections.defaultdict(list)
+        def connect(parent, child):
+            # both parent and child are not empty
+            if parent and child:
+                # building an undirected graph representation, assign the
+                # child value for the parent as the key and vice versa
+                conn[parent.val].append(child.val)
+                conn[child.val].append(parent.val)
+            # in-order traversal
+            if child.left: connect(child, child.left)
+            if child.right: connect(child, child.right)
+        # the initial parent node of the root is None
+        connect(None, root)
+        # start the breadth-first search from the target, hence the starting level is 0
+        queue = collections.deque()
+        queue.append(target.val)
+        visited = set()
+        visited.add(target.val)
+
+        for i in range(K):
+            level_size = len(queue)
+
+            while level_size != 0:
+                item = queue[0]
+                for child in conn[item]:
+                    
+                    if child not in visited:
+                        queue.append(child)
+                        visited.add(child)
+
+                queue.popleft()
+                level_size -= 1
+        return list(queue)

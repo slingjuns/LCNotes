4875296786787b1788ef20ad89760839c5702984
slingjun <slingjuns@gmail.com>
2024-02-04 00:00:00 -0500

    Commit 860 Lemonade Change.cpp

---
+++ Tags/Greedy/860 Lemonade Change/860 Lemonade Change.cpp
@@ -0,0 +1,25 @@
+// Date: February 4, 2024
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        auto comp = [](const int a, const int b) {return a > b;};
+        map<int, int,decltype(comp)> changes(comp);
+        for(int i = 0; i < bills.size(); ++i) {
+            int target = bills[i]-5;
+            for(map<int, int>::iterator it = changes.begin(); it != changes.end();) {
+                if(target == 0) break;
+                auto amount = it->first;
+
+                if(amount <= target) {
+                    target -= amount;
+                    changes[amount]--;
+                    if(changes[amount] == 0) it = changes.erase(it);
+                }
+                else it++;
+            }
+            if(target > 0) return false;
+            changes[bills[i]]++;
+        }
+        return true;
+    }
+};

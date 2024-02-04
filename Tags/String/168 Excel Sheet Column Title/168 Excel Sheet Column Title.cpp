26cf82f97a03234ea08e7c1e82bad5b9427d8332
slingjun <slingjuns@gmail.com>
2024-02-04 00:00:00 -0500

    Commit 168 Excel Sheet Column Title.cpp

---
+++ Tags/String/168 Excel Sheet Column Title/168 Excel Sheet Column Title.cpp
@@ -0,0 +1,17 @@
+// Date: February 4, 2024
+class Solution {
+public:
+    string convertToTitle(int n) {
+
+        string res;
+        char tmp;
+        while(n){
+            n--;
+            tmp = 'A' + n % 26;
+            res = tmp + res;
+            n /= 26;
+        }
+        return res;
+
+    }
+};
